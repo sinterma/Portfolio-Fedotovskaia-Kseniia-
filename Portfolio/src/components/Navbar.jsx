9c92d404589e6ef921cdf22d448af59821dd083a
Sosha Fedotovskaia <sinterma@gmail.com>
2025-05-07 23:49:36 +0200

    logo

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <nav className = "navbar">
             <Link to="/" className="logo">
-            <img src="./src/images/SQ-3.png" alt="Logo" />
+            <img src="./src/images/logo.png" alt="Logo" />
             </Link>
             <Link to = "/">Home</Link>
             <Link to = "/about">About</Link>
