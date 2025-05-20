755dc6def962dc7667971f562f1bcb6d0fc4f79c
Sosha Fedotovskaia <sinterma@gmail.com>
2025-05-20 21:58:13 +0200

    finished about page

--- Portfolio/src/pages/About.jsx
+++ Portfolio/src/pages/About.jsx
@@ -59,25 +59,26 @@ function About() {
       </div>
 
       <div className="about-content">
-        <h1>So, who am I?</h1>
-        <p>
-          <br />
-          I’m originally from Russia, but in 2023, due to the political situation, I made the difficult decision
-          to leave my country and start a new chapter in Germany. It was one of the biggest changes in my life,
-          but also one of the most rewarding.
-          <br /><br />
-          For the past five years, I worked as a QA Engineer, and this role taught me how to stay curious, detail-oriented,
-          and always open to learning. In May 2025, I completed a web development course and discovered a whole new passion.
-          Now I’m excited to combine my QA background with my fresh development skills and bring a well-rounded perspective
-          to building and testing software.
-          <br /><br />
-          My focus in web development is on JavaScript, TypeScript, React, and Node.js.
-          <br /><br />
-          Apart from tech, I’m a big animal lover with three cats who rule the house. In my free time, you’ll find me cycling,
-          cooking plant-based meals, designing websites, or getting lost in a good detective novel.
-          Music is always around me.
-        </p>
-      </div>
+  <h1>So, who am I?</h1>
+  <div className="about-text-box">
+    <p>
+      I’m originally from Russia, but in 2023, due to the political situation, I made the difficult decision
+      to leave my country and start a new chapter in Germany. It was one of the biggest changes in my life,
+      but also one of the most rewarding.
+      <br /><br />
+      For the past five years, I worked as a QA Engineer, and this role taught me how to stay curious, detail-oriented,
+      and always open to learning. In May 2025, I completed a web development course and discovered a whole new passion.
+      Now I’m excited to combine my QA background with my fresh development skills and bring a well-rounded perspective
+      to building and testing software.
+      <br /><br />
+      My focus in web development is on JavaScript, TypeScript, React, and Node.js.
+      <br /><br />
+      Apart from tech, I’m a big animal lover with three cats who rule the house. In my free time, you’ll find me cycling,
+      cooking plant-based meals, designing websites, or getting lost in a good detective novel.
+      Music is always around me.
+    </p>
+  </div>
+</div>
     </div>
   );
 }
